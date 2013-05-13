6a3cba5ae50157397766aee17356e96f3186d9ad
Andrei Rusu <andreirusu@deepmind.com>
2013-05-13 15:46:25 +0100

    Added zca-whitening functions.

---
+++ whitening.lua
@@ -0,0 +1,86 @@
+require 'pca'
+--
+-- ZCA-Whitening
+--
+-- Input: 
+--  - data tensor M x N1 [x N2 x ...] (required); at least 2D.
+--  - means: 1D tensor of size N = N1 x N2 x ... (flattned).
+--  - P: ZCA-transfor matrix of size N x N.
+--
+-- Behavior: 
+--  - if both means and P are provided, the ZCA-transformed data is returned, alongside means and P (unchanged). 
+--  - otherwise, means and P are computed and returned, preceded by the transformed data. 
+--
+-- Input arguments are never changed.
+--
+function unsup.zca_whiten(data, means, P)
+    local invP
+    local auxdata = data:clone()
+    local dims = data:size()
+    local nsamples = dims[1]
+    local n_dimensions = data:nElement() / nsamples
+    if data:dim() >= 3 then
+        auxdata:resize(nsamples, n_dimensions)
+    end
+    if not means and not P then
+        -- compute mean vector if not provided 
+        means = torch.mean(auxdata, 1):squeeze()
+        -- compute transformation matrix P if not provided
+        local ce, cv = unsup.pcacov(auxdata)
+        ce:add(1e-5):sqrt()
+        local invce = ce:clone():pow(-1)
+        local invdiag = torch.diag(invce)
+        P = torch.mm(cv, invdiag)
+        P = torch.mm(P, cv:t())
+
+        -- compute inverse of the transformation
+        local diag = torch.diag(ce)
+        invP = torch.mm(cv, diag)
+        invP = torch.mm(invP, cv:t())
+    end
+    -- remove the means
+    auxdata:add(torch.ger(torch.ones(nsamples), means):mul(-1))
+    -- transform in ZCA space
+    auxdata = torch.mm(auxdata, P)
+
+    auxdata:resizeAs(data)
+    return auxdata, means, P, invP
+end
+
+function unsup.zca_colour(data, means, P, invP)
+    local auxdata = data:clone()
+    local dims = data:size()
+    local nsamples = dims[1]
+    local n_dimensions = data:nElement() / nsamples
+    assert(params.means)
+    assert(params.invP)
+    if data:dim() >= 3 then
+        auxdata:resize(nsamples, n_dimensions)
+    end
+    -- remove the means
+    auxdata:add(torch.ger(torch.ones(nsamples), params.means):mul(-1))
+    -- transform in ZCA space
+    auxdata = torch.mm(auxdata, params.invP)
+
+    data:copy(auxdata:resizeAs(data))
+    return auxdata, means, P, invP
+end
+
+
+-- Function computes return a linear layer which applies a ZCA transform 
+-- to its input using a prec-computed (static) transformation matrix. 
+-- if not specified, the transformation parameters are computed from data
+function unsup.zca_layer(data, means, P, invP)
+    local auxdata
+    if not means and not P then 
+        auxdata, means, P, invP  = unsup.zca_whiten(data)
+    end
+    local layer = nn.Linear(data:size(2), data:size(2))
+    layer.weight:copy(P:t())
+    layer.bias:fill(0)
+    layer.bias:copy(layer:forward(means):mul(-1))
+    return layer, means, P, invP
+end
+
+
+
