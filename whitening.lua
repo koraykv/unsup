0511482e7a89f1d829b58da642b4d973607e70be
Andrei Rusu <andreirusu@deepmind.com>
2013-05-31 00:14:27 +0100

    Refactored zca_layer to handle 2D, 3D & 4D inputs.

--- whitening.lua
+++ whitening.lua
@@ -65,17 +65,27 @@ end
 
 
 -- Function computes return a linear layer which applies a ZCA transform 
--- to its input using a prec-computed (static) transformation matrix. 
+-- to its input using a precomputed (static) transformation matrix. 
 -- if not specified, the transformation parameters are computed from data
 function unsup.zca_layer(data, means, P, invP)
     local auxdata
-    if not means or not P then 
+    if not means or not P or not invP then 
         auxdata, means, P, invP  = unsup.zca_whiten(data)
     end
-    local layer = nn.Linear(data:size(2), data:size(2))
-    layer.weight:copy(P:t())
-    layer.bias:fill(0)
-    layer.bias:copy(layer:forward(means):mul(-1))
+    local n_dimensions = data:nElement() / data:size(1)
+    local linear = nn.Linear(n_dimensions, n_dimensions)
+    linear.weight:copy(P:t())
+    linear.bias:fill(0)
+    linear.bias:copy(linear:forward(means):mul(-1))
+    local layer
+    if data:nDimension() > 2 then 
+        layer = nn.Sequential()
+        layer:add(nn.Reshape(data:size(1), n_dimensions))
+        layer:add(linear)
+        layer:add(nn.Reshape(data:size()))
+    else
+        layer = linear
+    end
     return layer, means, P, invP
 end
 
