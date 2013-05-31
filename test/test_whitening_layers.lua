74fc2e14e08f229e913524c91fb00c78616db292
Andrei Rusu <andreirusu@deepmind.com>
2013-05-31 11:22:23 +0100

    Added a few convenience test functions which display whitened and coloured cifar10 data.

---
+++ test/test_whitening_layers.lua
@@ -0,0 +1,30 @@
+require 'torch'
+require 'torch-env'
+require 'dataset'
+require 'dataset/cifar10'
+require 'image'
+require 'unsup'
+
+
+
+ds = cifar10.raw_dataset().data:narrow(1,1,100):narrow(2,1,1):clone()
+collectgarbage()
+print({ds})
+image.display{image=ds, zoom=2, nrow=10}
+
+
+l, m, P, invP = unsup.zca_layer(ds)
+print(l)
+
+wds = l:forward(ds)
+print({wds})
+image.display{image=wds, zoom=2, nrow=10}
+
+invl = unsup.inv_zca_layer(ds, m, P, invP)
+print(invl)
+
+
+ods = invl:forward(wds)
+print({ods})
+image.display{image=ods, zoom=2, nrow=10}
+
