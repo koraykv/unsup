04f4fbd05b94d12ad8e51ae5eb1a1966b19c8573
soumith <soumith@gmail.com>
2016-01-25 06:22:02 -0500

    backward compatibility

--- init.c
+++ init.c
@@ -6,6 +6,24 @@
 #define unsup_(NAME) TH_CONCAT_3(unsup_, Real, NAME)
 #define torch_string_(NAME) TH_CONCAT_STRING_3(torch., Real, NAME)
 
+#if LUA_VERSION_NUM == 501
+/*
+** Adapted from Lua 5.2.0
+*/
+static void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup) {
+  luaL_checkstack(L, nup+1, "too many upvalues");
+  for (; l->name != NULL; l++) {  /* fill the table with given functions */
+    int i;
+    lua_pushstring(L, l->name);
+    for (i = 0; i < nup; i++)  /* copy upvalues to the top */
+      lua_pushvalue(L, -(nup+1));
+    lua_pushcclosure(L, l->func, nup);  /* closure with those upvalues */
+    lua_settable(L, -(nup + 3));
+  }
+  lua_pop(L, nup);  /* remove upvalues */
+}
+#endif
+
 #include "generic/util.c"
 #include "THGenerateFloatTypes.h"
 
